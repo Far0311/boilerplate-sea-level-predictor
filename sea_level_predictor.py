2946764df168d6ee65d02ca86cc31711880c86c9
Far0311 <farishaazmi98@gmail.com>
2024-03-15 09:09:19 +0000

    new file:   sea_level_plot.png

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import NumPy as np
+import numpy as np
 from scipy.stats import linregress
 
 def draw_plot():
@@ -8,11 +8,11 @@ def draw_plot():
     df = pd.read_csv('/workspace/boilerplate-sea-level-predictor/epa-sea-level.csv')
 
     # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], s = 20, marker="*", linewidths=0.0)
 
     # Create first line of best fit
     Line1 = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    x_val1 = np.arrange(df['Year'].min(), 2051, 1) # creates a NumPy array containing a sequence of numbers starting from the minimum value of the 'Year' column in the df and ending at 2050 (since the second argument is exclusive), with a step size of 1.
+    x_val1 = np.arange(df['Year'].min(), 2051, 1) # creates a NumPy array containing a sequence of numbers starting from the minimum value of the 'Year' column in the df and ending at 2050 (since the second argument is exclusive), with a step size of 1.
     y_val1 = x_val1*Line1.slope + Line1.intercept # corresponding y-value based on the slope 
 
     plt.plot(x_val1, y_val1)
@@ -21,15 +21,15 @@ def draw_plot():
     df_2000 = df[df['Year'] >= 2000] #df starting from year 2000
 
     Line2 = linregress(df_2000['Year'], df_2000['CSIRO Adjusted Sea Level'])
-    x_val2 = np.arrange(df_2000['Year'].min(), 2051, 1) # creates a NumPy array containing a sequence of numbers starting from the minimum value of the 'Year' column in the df and ending at 2050 (since the second argument is exclusive), with a step size of 1.
+    x_val2 = np.arange(df_2000['Year'].min(), 2051, 1) # creates a NumPy array containing a sequence of numbers starting from the minimum value of the 'Year' column in the df and ending at 2050 (since the second argument is exclusive), with a step size of 1.
     y_val2 = x_val2*Line2.slope + Line2.intercept # corresponding y-value based on the slope 
 
     plt.plot(x_val2, y_val2)
 
     # Add labels and title
     plt.xlabel('Year')
-    plt.ylabel('Sea Level (inches)')
-    plt.title('Rise in Sea Level')
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
